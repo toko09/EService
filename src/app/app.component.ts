f2a87dbe1565a0a69f1fbc28d19fd063fa8a06ed
Tornike Vakhtangidze <tornikevakhtangidze@gmail.com>
2024-01-14 17:57:59 +0400

    modified:   angular.json 	modified:   src/app/app.component.html 	deleted:    src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	new file:   src/app/components/header/header.component.css 	new file:   src/app/components/header/header.component.html 	new file:   src/app/components/header/header.component.ts 	new file:   src/app/components/landing/landing.component.css new file:   src/app/components/landing/landing.component.html 	new file:   src/app/components/landing/landing.component.ts 	new file:   src/assets/atv.png 	new file:   src/assets/mrelectric.webp 	new file:   src/assets/pls.webp 	modified:   src/favicon.ico 	modified:   src/index.html 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { LandingComponent } from "./components/landing/landing.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, LandingComponent]
 })
 export class AppComponent {
   title = 'EService';
---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css'
+})
+export class LandingComponent {
+  call() { 
+    window.location.href = 'tel:+995577227589'
+  }
+}
