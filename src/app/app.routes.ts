51e052fd99bcc40c33f8695972e7da893d69dffc
Tornike Vakhtangidze <tornikevakhtangidze@gmail.com>
2024-02-02 19:31:26 +0400

    clean up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { LandingComponent } from './components/landing/landing.component';
 
 export const routes: Routes = [
 
